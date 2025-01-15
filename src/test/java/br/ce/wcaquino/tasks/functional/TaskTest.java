299324763a5b375b84dc7a039b6b1bcbe6201cb0
Alan Michel <alan.pereira@skyone.solutions>
2025-01-14 21:55:06 -0300

    Implement parallelism with docker.

--- src/test/java/br/ce/wcaquino/tasks/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TaskTest.java
@@ -1,5 +1,7 @@
 package br.ce.wcaquino.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -7,75 +9,84 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TaskTest {
 	
-	public WebDriver accessApplication() {
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://localhost:8002/tasks/");
+	
+
+	public WebDriver accessApplication() throws MalformedURLException {
+		
+	    String ip = System.getenv("APP_IP");
+	    if (ip == null || ip.isEmpty()) {
+	        throw new IllegalStateException("The APP_IP environment variable has not been set!");
+	    }
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver driver = new RemoteWebDriver(new URL("http://" + ip + ":4444"), chromeOptions);
+		driver.navigate().to("http://" + ip + ":8002/tasks/");
 		driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
 		return driver;
 	}
 
 	@Test
-	public void shouldDontSaveTaskWithoutDescription () {
+	public void shouldDontSaveTaskWithoutDescription() throws MalformedURLException {
 		WebDriver driver = accessApplication();
-		
+
 		try {
-		driver.findElement(By.id("addTodo")).click();
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2040");
-		driver.findElement(By.id("saveButton")).click();
-		String message = driver.findElement(By.id("message")).getText();
-		Assert.assertEquals("Fill the task description", message);
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2040");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the task description", message);
 		} finally {
-			driver.quit();	
-		}
-		
-	}
-	
-	@Test
-	public void shouldSaveTaskSuccessfuly () {
-		WebDriver driver = accessApplication();
-		try {
-		driver.findElement(By.id("addTodo")).click();
-		driver.findElement(By.id("task")).sendKeys("Teste via selenium");
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/2040");
-		driver.findElement(By.id("saveButton")).click();
-		String message = driver.findElement(By.id("message")).getText();
-		Assert.assertEquals("Success!", message);
-		} finally {
-			driver.quit();	
+			driver.quit();
 		}
 	}
-	
+
 	@Test
-	public void shouldDontSaveTaskWithoutDate () {
+	public void shouldSaveTaskSuccessfuly() throws MalformedURLException {
 		WebDriver driver = accessApplication();
-		
 		try {
-		driver.findElement(By.id("addTodo")).click();
-		driver.findElement(By.id("task")).sendKeys("Teste via selenium");
-		driver.findElement(By.id("saveButton")).click();
-		String message = driver.findElement(By.id("message")).getText();
-		Assert.assertEquals("Fill the due date", message);
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2040");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
 		} finally {
-			driver.quit();	
+			driver.quit();
 		}
-		
 	}
-	
+
 	@Test
-	public void shouldSaveTaskWithPastDate () {
+	public void shouldDontSaveTaskWithoutDate() throws MalformedURLException {
+		WebDriver driver = accessApplication();
+
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the due date", message);
+		} finally {
+			driver.quit();
+		}
+
+	}
+
+	@Test
+	public void shouldSaveTaskWithPastDate() throws MalformedURLException {
 		WebDriver driver = accessApplication();
 		try {
-		driver.findElement(By.id("addTodo")).click();
-		driver.findElement(By.id("task")).sendKeys("Teste via selenium");
-		driver.findElement(By.id("dueDate")).sendKeys("10/10/1999");
-		driver.findElement(By.id("saveButton")).click();
-		String message = driver.findElement(By.id("message")).getText();
-		Assert.assertEquals("Due date must not be in past", message);
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/1999");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Due date must not be in past", message);
 		} finally {
-			driver.quit();	
+			driver.quit();
 		}
 	}
 }
